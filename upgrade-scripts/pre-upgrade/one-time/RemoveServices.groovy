27f30380afd768a5c5cd76caa0ca521df67e3366
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2022-12-06 17:01:34 +0000

    Revert "[EDP-DDM-21672] Fix services selectors"
    This reverts commit ccc4887cdec7d6d978e85a607b6e32d2dfd8a60b

--- upgrade-scripts/pre-upgrade/one-time/RemoveServices.groovy
+++
@@ -1,6 +0,0 @@
-void call() {
-    sh "oc -n $NAMESPACE delete svc citus-master 2> /dev/null || : &&" +
-            "oc -n $NAMESPACE delete svc citus-master-rep 2> /dev/null || : "
-}
-
-return this;
