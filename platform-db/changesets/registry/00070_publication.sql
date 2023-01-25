a99bdd00c82c3efe99f38620e6c854035a57b58e
Oleksandr Lazebnyi <oleksandr_lazebnyi@epam.com>
2023-01-25 11:43:25 +0200

    [EDP-DDM-23093] Added registry-template database
    Change-Id: Ide37499ead9296965f9e2ecbed06f6d0f24a3d44
    (cherry picked from commit 6465d100cd9d627a5a78bcae15bee57050c463d2)

--- platform-db/changesets/registry/00070_publication.sql
+++ platform-db/changesets/registry/00070_publication.sql
@@ -3,7 +3,7 @@
 -- Publication for Logical replication
 CREATE PUBLICATION analytical_pub /*FOR ALL TABLES*/ WITH (publish = 'insert, update, delete, truncate');
 
---changeset platform:recreate-logical-slot context:"pub"
+--changeset platform:recreate-logical-slot context:"pub and !template"
 -- Recreate logical replication slot for connect to last LSN after create publication command.
 SELECT pg_catalog.pg_drop_replication_slot('operational_sub') WHERE EXISTS (SELECT 1 FROM pg_catalog.pg_replication_slots WHERE slot_type = 'logical' AND slot_name = 'operational_sub' AND active='f');
 SELECT pg_catalog.pg_create_logical_replication_slot('operational_sub', 'pgoutput') WHERE NOT EXISTS (SELECT 1 FROM pg_catalog.pg_replication_slots WHERE slot_type = 'logical' AND slot_name = 'operational_sub');
