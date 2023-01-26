135659c600c5b34fcad5dfeeab9013795d0714c9
Oleksandr Lazebnyi <oleksandr_lazebnyi@epam.com>
2023-01-26 18:27:47 +0200

    [EDP-DDM-23101] Grant registry-regulation-management-role connect to registry DB
    Change-Id: I65d5bfbd80f1a918c58fcdfcad8ae18a81b20c83
    (cherry picked from commit 6d68a3b437525f55438afd17eb6a5e01a4f3aa6d)

--- platform-db/changesets/registry/00030_grant-connect.sql
+++ platform-db/changesets/registry/00030_grant-connect.sql
@@ -7,6 +7,10 @@ grant connect on database ${dbName} to ${appRoleName};
 grant connect on database ${dbName} to ${admRoleName};
 
 
+--changeset platform:registry-regulation-management-role-grant context:"pub"
+grant connect on database ${dbName} to ${regRegulationRoleName};
+
+
 --changeset platform:analytics-admin-role-grant context:"sub"
 grant connect on database ${dbName} to ${anAdmName};
 
